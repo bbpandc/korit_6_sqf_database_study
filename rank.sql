75c03852402e07f01e9315e2686e0a3a8f94d4f9
bbpandc <haeminsia@gmail.com>
2024-07-04 08:50:47 +0900

    rank

--- rank.sql
+++ rank.sql
@@ -6,13 +6,13 @@ from
 	(select
 		-- partition by: 순위를 매길 범위
         
-		-- rank(): publisher_id 안에서 book_count가 큰 수 부터 번호를 매긴다(수가 같으면 공동 순위로 -> 1등, 2등, 2등, 4등). 
+		-- publisher_id가 같은 값들 안에서 book_count가 큰 수 부터 번호를 매긴다(수가 같으면 공동 순위로 -> 1등, 2등, 2등, 4등). 
 	#	rank() over(partition by publisher_id order by book_count desc) as `rank`,
     
-		-- dense_rank(): publisher_id 안에서 book_count가 큰 수 부터 번호를 매긴다(수가 같으면 공동 순위로 -> 1등, 2등, 2등, 3등). 
+		-- publisher_id가 같은 값들 안에서 book_count가 큰 수 부터 번호를 매긴다(수가 같으면 공동 순위로 -> 1등, 2등, 2등, 3등). 
 	#	dense_rank() over(partition by publisher_id order by book_count desc) as `dense_rank`,
     
-		-- row_number(): publisher_id 안에서 book_count가 큰 수 부터 번호를 매긴다(수가 같으면 행의 순서대로 -> 순위 중복x).
+		-- publisher_id가 같은 값들 안에서 book_count가 큰 수 부터 번호를 매긴다(수가 같으면 행의 순서대로 -> 순위 중복x).
 		row_number() over(partition by publisher_id order by book_count desc) as `num`,
 		pc_count_tb.*
 	from
